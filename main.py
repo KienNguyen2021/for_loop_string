2c830619484a87ed0f035ca7359a0184e0910f21
kien1nguyen <>
2021-04-25 06:01:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+fruits = ["apple","banana","orange","grade"]
+
+for fruit in fruits: 
+    print (fruit)
+    print (fruit + " straberry ")
+print (fruits)    
+
+
+num_array = [2,3,4,1,0]
+for num in num_array:
+
+    print (num)
